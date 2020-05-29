a544eae653e61c65dfd79d3f14b4e562543e978d
Sergii Torkhov <torhov.s@gmail.com>
2020-05-30 00:53:44 +0200

    fix form

--- wp-content/themes/classy/assets/js/components/form-antispam.js
+++ wp-content/themes/classy/assets/js/components/form-antispam.js
@@ -8,17 +8,16 @@
  */
 const initWPCF7Antispam = () => {
     const key = '745643534543745634532';
-    const $input = $('input[name ="qqq"]');
 
     $('.wpcf7-form').append("<input type='hidden' name='qqq'/>");
 
-    $input.val(key);
+    $('input[name ="qqq"]').val(key);
 
     // To resubmit the form without reloading the page
     document.addEventListener('wpcf7submit', (event) => {
         setTimeout(() => {
             console.log('wpcf7submit');
-            $input.val(key);
+            $('input[name ="qqq"]').val(key);
         }, 1000);
 
     }, false);
